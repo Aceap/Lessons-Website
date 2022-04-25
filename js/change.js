42d8da77ce09d41f3a46985dd341cabed7c43215
Aceap <2860921959@qq.com>
2022-04-25 23:55:36 +0800

    version:0425 ppt.html未完成

--- js/change.js
+++ js/change.js
@@ -1,10 +1,32 @@
-function changeBackground() {
-    //轮播图片
-    index++;
-    let a = document.getElementsByClassName("img-inside");
-    if (index >= a.length) index = 0;
-    for (let i = 0; i < 3; i++) {
-        a[i].style.display = 'none';
+// function changeBackground() {
+//     //轮播图片
+//     index++;
+//     let a = document.getElementsByClassName("img-inside");
+//     if (index >= a.length) index = 0;
+//     for (let i = 0; i < 3; i++) {
+//         a[i].style.display = 'none';
+//     }
+//     a[index].style.display = 'block';
+// }
+
+let t = null;
+t = setTimeout(time, 500); //开始运行
+function time() {
+    clearTimeout(t); //清除定时器
+    let dt = new Date();
+    let y = dt.getFullYear();
+    let mt = dt.getMonth() + 1;
+    let day = dt.getDate();
+    let h = dt.getHours(); //获取时
+    let m = dt.getMinutes(); //获取分
+    let s = dt.getSeconds(); //获取秒
+    if (m < 10) {
+        m = '0' + m;
     }
-    a[index].style.display = 'block';
+    if (s < 10) {
+        s = '0' + s;
+    }
+    document.querySelector(".time").innerHTML =
+        y + "/" + mt + "/" + day + "-" + h + ":" + m + ":" + s;
+    t = setTimeout(time, 500); //设定定时器，循环运行
 }

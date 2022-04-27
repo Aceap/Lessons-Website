bf4fd25aa46bbac1e15dfc635ca3e5f6db0c2707
Aceap <2860921959@qq.com>
2022-04-27 21:17:25 +0800

    version:0427-final 添加留言功能用户名验证，同时精简代码

--- js/liuyan.js
+++ js/liuyan.js
@@ -1,26 +1,45 @@
 window.onload = function () {
-    var Name = document.getElementById("name");
-    var Msg = document.getElementById("msg");
-    var Btn = document.getElementById("btn");
-    var Ul = document.getElementById("msgInputted");
+    let Btn = document.getElementById("btn");
     Btn.onclick = function () {
-        var username = Name.value;
-        var MsgValue = Msg.value;
-        var Li = document.createElement("li");
+        let Name = document.getElementById("name");
+        Name.addEventListener("click", function () {
+            Name.style.color = "black";
+        })
+        let Msg = document.getElementById("msg");
+        let Ul = document.getElementById("msgInputted");
+
+        //用户名校验
+        let username = Name.value;
+        let ch = username.charAt(0);
+        if (ch >= "0" && ch <= "9") {
+            alert("用户名首字母不能是数字！");
+            Name.style.color = "red";
+            return;
+        }
+        if (username.length < 3 || username.length > 20) {
+            alert("用户名长度必须为3~20个字符！");
+            Name.style.color = "red";
+            return;
+        }
+
+        //添加留言
+        let MsgValue = Msg.value;
+        let Li = document.createElement("li");
         Li.innerHTML = username + ':' + MsgValue + " &nbsp;&nbsp;&nbsp;<button>删除</button><br>系统回复：感谢您的留言！";
-        var arrayLi = Ul.getElementsByTagName("li")
-        if (arrayLi.length > 0) {
+        let arrayLi = Ul.getElementsByTagName("li");
+        if (arrayLi.length > 0) { //判断是否已有留言
             Ul.insertBefore(Li, arrayLi[0]);
         } else {
             Ul.appendChild(Li);
         }
-        Msg.value = '';
-        var Span = document.getElementsByTagName("button");
-        for (var i = 0; i < Span.length; i++) {
-            Span[i].onclick = function () {
+        Msg.value = ''; //置空消息文本框
+
+        //删除留言
+        let del_btn = document.getElementsByTagName("button");
+        for (let i = 0; i < del_btn.length; i++) {
+            del_btn[i].addEventListener("click", function () {
                 Ul.removeChild(this.parentNode);
-                count--;
-            }
+            })
         }
     }
 }
